dbb4fadb3d8dc2555765278debeb45fdacd57d4b
Alfredo Vidinhas <alfredoezequiel2@gmail.com>
2021-11-15 07:27:03 +0100

    Commit Inicial

---
+++ kino-transports/kino_transportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  kino_transportsApp.swift
+//  kino-transports
+//
+//  Created by Alfredo Vidinhas on 15/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct kino_transportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
